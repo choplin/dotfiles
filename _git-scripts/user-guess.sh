c2d9d21d63283222e42c66ef14a08c7ff8fea541
choplin <choplin.choplin@gmail.com>
2015-09-22 10:47:58 +0900

    Set user.(name|email) for git automatically

---
+++ _git-scripts/user-guess.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# from http://willi.am/blog/2015/02/27/dynamically-configure-your-git-email/
+
+set -e
+set -u
+
+name=choplin
+email=choplin.choplin@gmail.com
+
+remote=$(git remote -v | awk '/\(push\)$/ {print $2}')
+
+if [ $(echo $remote | grep '.*stash\.dev\.scaleout\.jp.*') ]; then
+  name=okuno
+  email=okuno@scaleout.jp
+fi
+
+echo "Configuring user.name as $name"
+git config user.name $name
+
+echo "Configuring user.email as $email"
+git config user.email $email
