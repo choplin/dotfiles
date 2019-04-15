d49e3861d4785c5edf791bdb65610ba509215ae6
Akihiro Okuno <choplin.choplin@gmail.com>
2019-04-15 13:29:55 +0900

    Update env.fish

--- fish/env.fish
+++ fish/env.fish
@@ -1,23 +1,14 @@
 set theme_color_scheme solarized-light
 
-set -x JAVA_HOME (/usr/libexec/java_home)
 set -x ANT_OPTS -Dfile.encoding=UTF8
-
 set -x DEFAULT_USER okuno
-
 set -x MAVEN_OPTS "-Dfile.encoding=UTF-8"
-
 set -x EDITOR vim
 set -x VISUAL vim
 
-set -x RUST_SRC_PATH $HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
-
-set -x BROWSER open
-
 set -U fish_user_paths
-
 if which go > /dev/null
-    set -x GOROOT /usr/local/opt/go/libexec
+    set -x GOROOT (go env GOROOT)
     set -x GOPATH $HOME/.go
     set -U fish_user_paths $GOROOT/bin $GOPATH/bin $fish_user_paths
 end
@@ -30,18 +21,30 @@ if which direnv > /dev/null
     eval (direnv hook fish)
 end
 
-if test -d $HOME/.cargo
-    set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-end
+switch (uname)
+    case Linux
+        if test -d /home/linuxbrew/.linuxbrew
+            begin
+                set -l brew_prefix (/home/linuxbrew/.linuxbrew/bin/brew --prefix)
+                set -U fish_user_paths $brew_prefix/bin $brew_prevfix/sbin $fish_user_paths
+            end
+        end
+        set -x DOCKER_HOST tcp://localhost:2375
+        umask 022
+    case Darwin
+        set -x RUST_SRC_PATH $HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
+        set -x BROWSER open
+        if type -q /usr/libexec/java_home
+            set -x JAVA_HOME (/usr/libexec/java_home)
+        end
+        begin
+            set -l brew_prefix (brew --prefix)
+            set -U fish_user_paths $brew_prefix/bin $brew_prevfix/sbin $fish_user_paths
 
-begin
-    set -l brew_prefix (brew --prefix)
-
-    if test -d $brew_prefix/sbin
-        set -U fish_user_paths $fish_user_paths $brew_prefix/sbin
-    end
-
-    if test -d $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
-        . $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-    end
+            if test -d $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+                source $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+            end
+        end
+    case FreeBSD NetBSD DragonFly
+    case '*'
 end
