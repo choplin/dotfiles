e6f1df9edc033afe8b667d45fd5a5da686e28492
Akihiro Okuno <choplin.choplin@gmail.com>
2019-04-16 16:23:08 +0900

    Remove mac specific aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,15 +1,1 @@
 alias vim nvim
-
-if set -q TMUX
-  alias pbcopy "reattach-to-user-namespace pbcopy"
-end
-
-alias ap "ansible-playbook -i hosts/hosts --skip-tags commit_check"
-
-function idea
-    open -a "/Applications/IntelliJ IDEA.app/Contents/MacOS/idea" (pwd)
-end
-
-function clion
-    open -a "/Applications/CLion.app/Contents/MacOS/clion" (pwd)
-end
