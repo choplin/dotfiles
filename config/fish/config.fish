92bf64fe469a463bd7b500190976214a677b21c1
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-06 10:36:52 +0900

    Update fish configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,32 @@
-# alias & function
-. ~/.config/fish/aliases.fish
+set fish_greeting ""
 
-# environment variables
+set -U brew_prefix
+switch (uname)
+    case Linux
+        set -U brew_prefix /home/linuxbrew/.linuxbrew
+    case Darwin
+        set -U brew_prefix /opt/homebrew
+    case '*'
+end
+
+
+if test -e $brew_prefix
+    fish_add_path $brew_prefix/bin
+    fish_add_path $brew_prefix/sbin
+end
+
 if status --is-login
     . ~/.config/fish/env.fish
+end
+
+if status is-interactive
+    . ~/.config/fish/aliases.fish
 end
 
 if test -e ~/.config/fish/local.fish
     . ~/.config/fish/local.fish
 end
 
-set fish_greeting ""
-
-#export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+if type starship
+    starship init fish | source
+end
--- config/fish/env.fish
+++ config/fish/env.fish
@@ -1,50 +1,16 @@
-set theme_color_scheme solarized-light
-
-set -x ANT_OPTS -Dfile.encoding=UTF8
-set -x DEFAULT_USER okuno
-set -x MAVEN_OPTS "-Dfile.encoding=UTF-8"
-set -x EDITOR vim
-set -x VISUAL vim
-
-set -U fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+fish_add_path $HOME/.local/bin $fish_user_paths
 
 switch (uname)
     case Linux
-        if test -d /home/linuxbrew/.linuxbrew
-            begin
-                set -l brew_prefix (/home/linuxbrew/.linuxbrew/bin/brew --prefix)
-                set -U fish_user_paths $brew_prefix/bin $brew_prevfix/sbin $fish_user_paths
-                #set -x SHELL /home/linuxbrew/.linuxbrew/bin/fish
-            end
-        end
-        #set -x DOCKER_HOST tcp://localhost:2375
-        #umask 022
     case Darwin
-        set -x RUST_SRC_PATH $HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
-        set -x BROWSER open
-        if type -q /usr/libexec/java_home
-            set -x JAVA_HOME (/usr/libexec/java_home)
-        end
-        begin
-            set -l brew_prefix (brew --prefix)
-            set -U fish_user_paths $brew_prefix/bin $brew_prevfix/sbin $fish_user_paths
-
-            if test -d $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
-                source $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-            end
+        if test -d $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+            source $brew_prefix/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
         end
     case '*'
 end
 
-if which go > /dev/null
-    set -x GOROOT (go env GOROOT)
-    set -x GOPATH $HOME/.go
-    set -U fish_user_paths $GOROOT/bin $GOPATH/bin $fish_user_paths
-end
-
 if test -d $HOME/.cargo
-    set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+    fish_add_path $HOME/.cargo/bin
 end
 
 if which direnv > /dev/null
