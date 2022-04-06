dfe1071d5367c0363eff7cee60c9145454671164
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-06 18:34:17 +0900

    Use up/down-line-or-beginning-search

--- zsh/keybinding.zsh
+++ zsh/keybinding.zsh
@@ -1,4 +1,8 @@
 bindkey -e
 
-bindkey '^p' history-beginning-search-backward
-bindkey '^n' history-beginning-search-forward
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^P" up-line-or-beginning-search
+bindkey "^N" down-line-or-beginning-search
