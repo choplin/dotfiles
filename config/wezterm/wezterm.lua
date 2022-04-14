b2100af3cc4242d6c50c33b5c77c2d637ebd0296
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-14 11:33:49 +0900

    Update wezterm font settings

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font("HackgenNerd Console"),
-  font_size = 18,
+  font = wezterm.font("Hackgen35Nerd Console"),
+  font_size = 16.0,
   color_scheme = "OneHalfDark",
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
@@ -12,7 +12,7 @@ return {
   use_fancy_tab_bar = true,
   window_frame = {
     font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-    font_size = 16.0,
+    font_size = 12.0,
     active_titlebar_bg = "#333333",
     inactive_titlebar_bg = "#111111",
   },
