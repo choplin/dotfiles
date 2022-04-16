35496e4029409e2a2e257ba06077f9608aae94ae
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-16 18:20:31 +0900

    Forgot to add lua modules for lvim

---
+++ config/lvim/lua/scrollbar_ext.lua
@@ -0,0 +1,14 @@
+local utils = require("scrollbar.utils")
+local render = require("scrollbar").render
+
+local M = {}
+
+function M.hide_search_results()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local scrollbar_marks = utils.get_scrollbar_marks(bufnr)
+  scrollbar_marks.search = nil
+  utils.set_scrollbar_marks(bufnr, scrollbar_marks)
+  render()
+end
+
+return M
