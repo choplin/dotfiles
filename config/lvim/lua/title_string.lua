69c8049eceec8da86e2f5076a0ceca5ad252b823
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-17 14:08:01 +0900

    Use project_nvim to get a project root

--- config/lvim/lua/title_string.lua
+++ config/lvim/lua/title_string.lua
@@ -1,46 +1,19 @@
 local M = {}
 
-local project_dir_patterns = { ".git", "go.mod" }
-local function find_project_dir(path)
-  local dir
-  if vim.fn.isdirectory(path) == 0 then
-    dir = vim.fn.fnamemodify(path, ":h")
-  else
-    dir = path
-  end
-
-  local home_dir = vim.fn.fnamemodify("~", ":p")
-  while true do
-    for _, pat in pairs(project_dir_patterns) do
-      if vim.fn.filewritable(dir .. "/" .. pat) > 0 then
-        return dir
-      end
-    end
-    local next = vim.fn.fnamemodify(dir, ":h")
-    if next == home_dir or next == dir then
-      break
-    else
-      dir = next
-    end
-  end
-  return nil
-end
-
 local function title_string()
-  local path = vim.fn.expand("%:p")
-  local home_dir = vim.fn.fnamemodify("~", ":p")
+  local path = vim.fn.expand("%:~")
   -- -- return the file path if the file exists outside home directory
-  if not vim.startswith(path, home_dir) then
+  if not vim.startswith(path, "~") then
     return path
   end
 
-  local project_dir = find_project_dir(path)
+  local project_root = require('project_nvim.project').get_project_root()
   -- return the file path relative to the home directory if the file exists outside project directory
-  if project_dir == nil then
-    return vim.fn.fnamemodify(path, ":~")
+  if project_root == nil then
+    return path
   end
   -- return the project directory name if found
-  return vim.fn.fnamemodify(project_dir, ":t")
+  return vim.fn.fnamemodify(project_root, ":t")
 end
 
 function M.title_string()
