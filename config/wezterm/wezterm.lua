3251a41d2d3200f7fa6bf153009653d0b8b0b9f8
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-17 21:30:28 +0900

    Update weaterm settings

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,12 +7,13 @@ return {
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
+  audible_bell = "Disabled",
 
-  tab_bar_at_bottom = false,
+  tab_bar_at_bottom = true,
   use_fancy_tab_bar = true,
   window_frame = {
     font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-    font_size = 12.0,
+    font_size = 14.0,
     active_titlebar_bg = "#333333",
     inactive_titlebar_bg = "#111111",
   },
@@ -23,4 +24,6 @@ return {
     top = 0,
     bottom = 0,
   },
+
+  send_composed_key_when_right_alt_is_pressed = false,
 }
