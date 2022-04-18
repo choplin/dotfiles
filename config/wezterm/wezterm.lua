e6cf4239edab38fd67c00fac9d03f1857d7ad58e
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-18 16:15:12 +0900

    Define severl wezterm keymaps for manipulation of pane

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,30 @@
 local wezterm = require 'wezterm';
 
+local function merge(...)
+  local l = {}
+  for _, list in ipairs({ ... }) do
+    for _, v in ipairs(list) do
+      table.insert(l, v)
+    end
+  end
+  return l
+end
+
+local function map(list, f)
+  local l = {}
+  for _, v in ipairs(list) do
+    table.insert(l, f(v))
+  end
+  return l
+end
+
+local direction_keys = {
+  { key = "h", direction = "Left" },
+  { key = "l", direction = "Right" },
+  { key = "k", direction = "Up" },
+  { key = "j", direction = "Down" },
+}
+
 return {
   font = wezterm.font("Hackgen35Nerd Console"),
   font_size = 16.0,
@@ -26,4 +51,45 @@ return {
   },
 
   send_composed_key_when_right_alt_is_pressed = false,
+
+  leader = { key = "y", mods = "CTRL", timeout_milliseconds = 1000 },
+  keys = merge(
+    {
+      { key = "v", mods = "LEADER|CTRL", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+      { key = "d", mods = "SUPER", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+      { key = "s", mods = "LEADER|CTRL", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+      { key = "o", mods = "LEADER|CTRL", action = wezterm.action { ActivatePaneDirection = "Next" } },
+    },
+    map(direction_keys, function(e)
+      return { key = e.key, mods = "LEADER", action = wezterm.action {
+        Multiple = {
+          wezterm.action { ActivatePaneDirection = e.direction },
+          wezterm.action { ActivateKeyTable = { name = "pane", one_shot = false } },
+        }
+      } }
+    end),
+    map(direction_keys, function(e)
+      return { key = e.key, mods = "LEADER|SHIFT", action = wezterm.action {
+        Multiple = {
+          wezterm.action { AdjustPaneSize = { e.direction, 5 } },
+          wezterm.action { ActivateKeyTable = { name = "pane", one_shot = false } },
+        }
+      } }
+    end)
+  ),
+  key_tables = {
+    pane = {
+      { key = "h", action = wezterm.action { ActivatePaneDirection = "Left" } },
+      { key = "l", action = wezterm.action { ActivatePaneDirection = "Right" } },
+      { key = "k", action = wezterm.action { ActivatePaneDirection = "Up" } },
+      { key = "j", action = wezterm.action { ActivatePaneDirection = "Down" } },
+
+      { key = "H", mods = "SHIFT", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+      { key = "J", mods = "SHIFT", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+      { key = "K", mods = "SHIFT", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+      { key = "L", mods = "SHIFT", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+
+      { key = "Escape", action = "PopKeyTable" },
+    },
+  }
 }
