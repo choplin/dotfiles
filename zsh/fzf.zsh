0eaf8fba2163ea248fe58ff99c13ab6a25b47f42
Akihiro Okuno <choplin.choplin@gmail.com>
2022-04-26 10:25:00 +0900

    Set fzf options for ghq-fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -6,8 +6,9 @@ fi
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+local fzf_options=('--height=40%' '--info=inline' '--layout=reverse' '--select-1' '--exit-0')
 function ghq-fzf() {
-  local selected_dir=$(ghq list | fzf --query="$LBUFFER")
+  local selected_dir=$(ghq list | fzf --query="$LBUFFER" $fzf_options)
 
   if [ -n "$selected_dir" ]; then
     BUFFER="cd $(ghq root)/${selected_dir}"
