757161ab1f0d9c47b4e365769d7cca41eb75b99f
Akihiro Okuno <choplin.choplin@gmail.com>
2022-05-01 21:37:25 +0900

    Update fzf options for ghq-fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -8,10 +8,11 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 local fzf_options=('--height=40%' '--info=inline' '--layout=reverse' '--select-1' '--exit-0')
 function ghq-fzf() {
-  local selected_dir=$(ghq list | fzf --query="$LBUFFER" $fzf_options)
+  local root=$(ghq root)
+  local selected_dir=$(ghq list | fzf --query="$LBUFFER" $fzf_options --preview="ls ${root}/{}")
 
   if [ -n "$selected_dir" ]; then
-    BUFFER="cd $(ghq root)/${selected_dir}"
+    BUFFER="cd ${root}/${selected_dir}"
     zle accept-line
   fi
 
