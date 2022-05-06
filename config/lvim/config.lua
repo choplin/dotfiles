474307fe2b62fa2f5db5d53b709f22751a41692a
Akihiro Okuno <choplin.choplin@gmail.com>
2022-05-06 21:26:01 +0900

    Add several nvim plugin
    - nvim-config-local to load local nvim config
    - nvim-metals for better scala support

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -432,7 +432,59 @@ lvim.plugins = {
       ['style'] = 'minimal'
     }
   end },
-  { "ron-rs/ron.vim" }
+  { "ron-rs/ron.vim" },
+  {
+    "klen/nvim-config-local", config = function()
+      require('config-local').setup {
+        lookup_parents = true,
+        config_files = { ".vimrc.lua" }
+      }
+    end
+  },
+  { 'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }, config = function()
+    local metals_config = require("metals").bare_config()
+    metals_config.settings = {
+      showImplicitArguments = true,
+      excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+    }
+
+    metals_config.init_options.statusBarProvider = "on"
+    table.insert(lvim.builtin.lualine.sections.lualine_c, 'g:metals_status')
+    require("lvim.core.lualine").setup()
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    metals_config.capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+    require("dap").configurations.scala = {
+      {
+        type = "scala",
+        request = "launch",
+        name = "RunOrTest",
+        metals = {
+          runType = "runOrTestFile",
+        },
+      },
+      {
+        type = "scala",
+        request = "launch",
+        name = "Test Target",
+        metals = {
+          runType = "testTarget",
+        },
+      },
+    }
+    metals_config.on_attach = function(client, bufnr)
+      require("metals").setup_dap()
+    end
+
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "scala", "sbt", "java" },
+      callback = function()
+        require("metals").initialize_or_attach(metals_config)
+      end,
+      group = nvim_metals_group,
+    })
+  end }
 }
 
 
