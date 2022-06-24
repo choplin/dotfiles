f94f6aba9249a0082c28908e3eddfb657b0f2d0f
Akihiro Okuno <choplin.choplin@gmail.com>
2022-06-24 18:09:35 +0900

    Enable yaml formatting

--- config/lvim/ftplugin/yaml.lua
+++ config/lvim/ftplugin/yaml.lua
@@ -1,4 +1,4 @@
-require("lvim.lsp.manager").setup("jsonls", {
+require("lvim.lsp.manager").setup("yamlls", {
   on_attach = function(client, bufnr)
     client.resolved_capabilities.document_formatting = true
     require("lvim.lsp").common_on_attach(client, bufnr)
