86858f27cb91f43dbc612cfc6f6660b57e2f8c3e
Akihiro Okuno <choplin.choplin@gmail.com>
2022-07-06 10:38:35 +0900

    Revise neotest settings

--- config/lvim/lua/plugins/debug.lua
+++ config/lvim/lua/plugins/debug.lua
@@ -5,8 +5,25 @@ return {
     requires = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim"
-    }
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-neotest/neotest-plenary",
+      "nvim-neotest/neotest-vim-test",
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-go",
+    },
+    config = function() require("neotest").setup({
+        adapters = {
+          require("neotest-go"),
+          require("neotest-python")({
+            dap = { justMyCode = false },
+          }),
+          require("neotest-plenary"),
+          require("neotest-vim-test")({
+            ignore_file_types = { "python", "vim", "lua", "go" },
+          }),
+        }
+      })
+    end,
   },
   -- Provide UI for nvim-dap
   { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } },
