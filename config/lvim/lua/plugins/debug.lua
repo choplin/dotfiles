be4e2553a4994c13ee6dedd2cd599b892b70637e
Akihiro Okuno <choplin.choplin@gmail.com>
2022-08-16 16:36:38 +0900

    Add vim-test plugin

--- config/lvim/lua/plugins/debug.lua
+++ config/lvim/lua/plugins/debug.lua
@@ -28,4 +28,5 @@ return {
   -- Provide UI for nvim-dap
   { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } },
   -- { "theHamsta/nvim-dap-virtual-text" },
+  { "vim-test/vim-test" },
 }
