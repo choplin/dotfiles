1014a82276471f21ee4a7c129cb7ac75e09e4eb0
Akihiro Okuno <choplin.choplin@gmail.com>
2022-08-17 16:05:21 +0900

    Use mason's API to set null-ls linters and formatters

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -154,29 +154,24 @@ vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, {
 -- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
-}
+do
+  local null_ls = require("null-ls")
+  local mason_package = require("mason-core.package")
 
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+  local formatter_sources = {}
+  local linter_sources = {}
+  for _, package in ipairs(require("mason-registry").get_installed_packages()) do
+    local category = package.spec.categories[1]
+    if category == mason_package.Cat.Formatter then
+      table.insert(formatter_sources, { name = package.name })
+    elseif category == mason_package.Cat.Linter then
+      table.insert(linter_sources, { name = package.name })
+    end
+  end
+
+  require("lvim.lsp.null-ls.formatters").setup(formatter_sources)
+  require("lvim.lsp.null-ls.linters").setup(linter_sources)
+end
 
 lvim.plugins = require('util').concatLists(
   require('plugins/base'),
