6e08bfc32f3934924f7f2da6d4a4dba1055d41f4
Akihiro Okuno <choplin.choplin@gmail.com>
2022-08-18 12:11:55 +0900

    Remove comments

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -44,28 +44,6 @@ vim.keymap.set("n", "k", "gk")
 vim.keymap.set("n", "<Down>", "gj")
 vim.keymap.set("n", "<Up>", "gk")
 
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = false
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
 lvim.builtin.telescope = vim.tbl_deep_extend("keep", {
   defaults = {
     layout_config = {
