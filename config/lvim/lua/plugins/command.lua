905c44419189f8e91b274184eb86272c233acb03
Akihiro Okuno <choplin.choplin@gmail.com>
2022-09-07 17:06:12 +0900

    Remove unused settings of Trouble

--- config/lvim/lua/plugins/command.lua
+++ config/lvim/lua/plugins/command.lua
@@ -59,19 +59,6 @@ return {
           w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
         },
       }
-
-      do
-        local trouble = require "trouble.providers.telescope"
-        local telescope = require "telescope"
-        telescope.setup {
-          defaults = {
-            mappings = {
-              i = { ["<c-t>"] = trouble.open_with_trouble },
-              n = { ["<c-t>"] = trouble.open_with_trouble },
-            },
-          },
-        }
-      end
     end,
   },
   -- Symbols Outline
