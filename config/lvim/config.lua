6c68e3f5d92f508bf16f05560d78ff9aab5f2282
Akihiro Okuno <choplin.choplin@gmail.com>
2022-09-09 11:31:33 +0900

    Temporary workaround for bufferline.nvim
    see https://github.com/LunarVim/LunarVim/issues/2993

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -190,3 +190,6 @@ vim.filetype.add {
     ["Tiltfile"] = "tiltfile",
   },
 }
+
+lvim.builtin.bufferline.options.indicator_icon = nil
+lvim.builtin.bufferline.options.indicator = { style = "icon", icon = "▎" }
