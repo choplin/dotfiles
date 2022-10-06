f61b13f0ca865c993c6f933a9ce948f1f177be08
Akihiro Okuno <akihiro.okuno@alpaca.markets>
2022-10-06 22:34:17 +0900

    Change colorscheme to tokyonight-moon

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "tokyonight-storm"
+lvim.colorscheme = "tokyonight-moon"
 
 vim.opt.list = true
 vim.opt.listchars:remove "tab"
