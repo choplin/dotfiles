52df20ef6aa2c0cf634cfc2351d0a813e14ab132
Akihiro Okuno <akihiro.okuno@alpaca.markets>
2022-10-07 18:54:30 +0900

    Show git signs in scroll bar

--- config/lvim/lua/plugins/base.lua
+++ config/lvim/lua/plugins/base.lua
@@ -21,12 +21,62 @@ return {
     "petertriho/nvim-scrollbar",
     after = { "nvim-hlslens" },
     config = function()
-      require("scrollbar").setup()
+      local colors = require("tokyonight.colors").setup()
+      require("scrollbar").setup {
+        marks = {
+          GitAdd = {
+            text = { "▎" },
+            color = colors.green,
+            priority = -1,
+          },
+          GitDelete = {
+            text = { "▎" },
+            color = colors.red,
+            priority = -1,
+          },
+          GitChange = {
+            text = { "▎" },
+            color = colors.blue,
+            priority = -1,
+          },
+          GitChangeDelete = {
+            text = { "▎" },
+            color = colors.orange,
+            priority = -1,
+          },
+        },
+      }
       require("scrollbar.handlers.search").setup()
       vim.keymap.set("n", "<Esc>", function()
         require("scrollbar_ext").hide_search_results()
         vim.cmd "nohlsearch"
       end, {})
+      require("scrollbar.handlers").register("git", function(bufnr)
+        local gitsign = require "gitsigns"
+        local gitsign_hunks = require "gitsigns.hunks"
+        local colors_type = {
+          add = "GitAdd",
+          delete = "GitDelete",
+          change = "GitChange",
+          changedelete = "GitChangeDelete",
+        }
+        local nb_lines = vim.api.nvim_buf_line_count(bufnr)
+        local lines = {}
+        local hunks = gitsign.get_hunks(bufnr)
+        if hunks then
+          for _, hunk in ipairs(hunks) do
+            hunk.vend = math.min(hunk.added.start, hunk.removed.start) + hunk.added.count + hunk.removed.count
+            local signs = gitsign_hunks.calc_signs(hunk, 0, nb_lines)
+            for _, sign in ipairs(signs) do
+              table.insert(lines, {
+                line = sign.lnum,
+                type = colors_type[sign.type],
+              })
+            end
+          end
+        end
+        return lines
+      end)
     end,
   },
   -- Apply .editorconfig
