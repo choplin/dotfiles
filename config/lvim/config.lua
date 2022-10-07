7ecd18a94579b1c63bedc595166b759961f1d7f3
Akihiro Okuno <akihiro.okuno@alpaca.markets>
2022-10-07 18:54:50 +0900

    Show more hints with which-key

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -70,7 +70,15 @@ end
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["sg"] = { "<cmd>Telescope ghq list<cr>", "ghq list" }
 lvim.builtin.which_key.setup.plugins.presets.text_objects = true
-
+lvim.builtin.which_key.setup.plugins.presets = {
+  operators = true, -- adds help for operators like d, y, ...
+  motions = true, -- adds help for motions
+  text_objects = true, -- help for text objects triggered after entering an operator
+  windows = true, -- default bindings on <c-w>
+  nav = true, -- misc bindings to work with windows
+  z = true, -- bindings for folds, spelling and others prefixed with z
+  g = false, -- bindings for prefixed with g
+}
 lvim.builtin.which_key.mappings["B"] = lvim.builtin.which_key.mappings["b"]
 lvim.builtin.which_key.mappings["b"] = {
   function()
