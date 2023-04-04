fd2188314ff0495d339667c21ac01c80bbc15e2c
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-04 22:02:00 +0900

    Use mason-null-ls to setup null-ls sources

---
+++ config/nvim.lazyvim/lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = {
+      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      sources = {},
+    },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = {},
+        automatic_installation = false,
+        automatic_setup = true,
+        handlers = {},
+      })
+    end,
+  },
+}
