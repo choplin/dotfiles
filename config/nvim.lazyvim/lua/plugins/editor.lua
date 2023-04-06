0c629e7e8a09f8f9469293133b29bc5cabbc6f2d
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-07 00:24:37 +0900

    Add neogen

--- config/nvim.lazyvim/lua/plugins/editor.lua
+++ config/nvim.lazyvim/lua/plugins/editor.lua
@@ -1,4 +1,13 @@
 return {
   -- :SnipRun runs code instantly
   { "michaelb/sniprun", build = "bash ./install.sh", cmd = "SnipRun" },
+  -- :Neogen generates documentation comments
+  {
+    "danymat/neogen",
+    cmd = "Neogen",
+    config = function()
+      require("neogen").setup({ snippet_engine = "luasnip" })
+    end,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
 }
