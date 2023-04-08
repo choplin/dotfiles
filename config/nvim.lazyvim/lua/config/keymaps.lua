fc7a2d7b061f118997159e7abea4c15a306678d5
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-08 23:42:16 +0900

    Do not move cursor when searching with * or #

--- config/nvim.lazyvim/lua/config/keymaps.lua
+++ config/nvim.lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "*", function()
+  vim.cmd("normal! *")
+  local res = vim.fn.searchcount()
+  if res.total > 1 then
+    vim.cmd("normal! N")
+  end
+end, { silent = true })
+
+vim.keymap.set("n", "#", function()
+  vim.cmd("normal! #")
+  local res = vim.fn.searchcount()
+  if res.total > 1 then
+    vim.cmd("normal! N")
+  end
+end, { silent = true })
