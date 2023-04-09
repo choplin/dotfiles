953f8d218c079540d5e37f966279021dbfe9e34e
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-09 11:39:38 +0900

    Add several plugins for markdown

---
+++ config/nvim.lazyvim/lua/plugins/filetype.lua
@@ -0,0 +1,6 @@
+return {
+  -- Provide markdown preview in browser
+  { "iamcco/markdown-preview.nvim", build = "cd app && yarn install", ft = "markdown" },
+  -- Markdown table
+  { "dhruvasagar/vim-table-mode", ft = "markdown" },
+}
