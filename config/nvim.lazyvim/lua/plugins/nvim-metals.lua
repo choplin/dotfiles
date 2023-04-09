c9f89ddd90ea4bef70a1a15ffdd22d9d28863660
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-09 23:13:55 +0900

    Add nvim-metals

---
+++ config/nvim.lazyvim/lua/plugins/nvim-metals.lua
@@ -0,0 +1,51 @@
+return {
+  -- Scala
+  {
+    "scalameta/nvim-metals",
+    ft = { "scala", "sbt" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      }
+
+      metals_config.init_options.statusBarProvider = "on"
+
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("dap").configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+          },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
+        },
+      }
+
+      metals_config.on_attach = function(_, _)
+        require("metals").setup_dap()
+      end
+
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "scala", "sbt" },
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
+}
