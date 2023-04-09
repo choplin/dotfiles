4d28e4198e9d5937e0d0d8535a7fc7d0339c9508
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-09 11:20:31 +0900

    Add align.nvim

--- config/nvim.lazyvim/lua/plugins/editor.lua
+++ config/nvim.lazyvim/lua/plugins/editor.lua
@@ -45,4 +45,51 @@ return {
     "gennaro-tedesco/nvim-jqx",
     cmd = { "JqxList", "JqxQuery" },
   },
+  -- Align text
+  {
+    "Vonr/align.nvim",
+    init = function()
+      local ok, wk = pcall(require, "which-key")
+      if ok then
+        wk.register({
+          mode = { "x" },
+          ["<leader>A"] = { name = "+Align" },
+        })
+      end
+    end,
+    keys = {
+      {
+        "<leader>Aa",
+        function()
+          require("align").align_to_char(1, true)
+        end,
+        mode = { "x" },
+        desc = "Aligns to 1 character, looking left",
+      },
+      {
+        "<leader>As",
+        function()
+          require("align").align_to_char(2, true, true)
+        end,
+        mode = { "x" },
+        desc = "Aligns to 2 characters, looking left and with previews",
+      },
+      {
+        "<leader>Aw",
+        function()
+          require("align").align_to_string(false, true, true)
+        end,
+        mode = { "x" },
+        desc = "Aligns to a string, looking left and with previews",
+      },
+      {
+        "<leader>Ar",
+        function()
+          require("align").align_to_string(true, true, true)
+        end,
+        mode = { "x" },
+        desc = "Aligns to a Lua pattern, looking left and with previews",
+      },
+    },
+  },
 }
