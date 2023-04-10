8ea322d8d5c5f34808b555e6e1ff2bf6d54e8962
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-10 12:30:23 +0900

    Add nvim-treesitter-context

--- config/nvim.lazyvim/lua/plugins/treesitter.lua
+++ config/nvim.lazyvim/lua/plugins/treesitter.lua
@@ -11,4 +11,11 @@ return {
       },
     },
   },
+  -- Show current context at the top of the windows as virtual text
+  {
+    "lewis6991/nvim-treesitter-context",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
+  },
 }
