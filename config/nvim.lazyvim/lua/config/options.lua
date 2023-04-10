23d9e5dfd75f78d971f1898cde8b32f21303421d
Akihiro Okuno <choplin.choplin@gmail.com>
2023-04-10 13:44:35 +0900

    Set several vim options

--- config/nvim.lazyvim/lua/config/options.lua
+++ config/nvim.lazyvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+opt.listchars:remove("tab")
+opt.listchars:append("tab:￫ ")
+opt.listchars:append("extends:»")
+opt.listchars:append("precedes:«")
+opt.timeoutlen = 250
+opt.guifont = "HackGen35 Console NFJ:h18"
