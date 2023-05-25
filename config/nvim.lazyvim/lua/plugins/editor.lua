0b69efc4a8b2ae978d2a3a839bb6724bd75ffb4c
Akihiro Okuno <choplin.choplin@gmail.com>
2023-05-25 16:16:13 +0900

    Fix conflicted keymap to delete bufeer

--- config/nvim.lazyvim/lua/plugins/editor.lua
+++ config/nvim.lazyvim/lua/plugins/editor.lua
@@ -133,7 +133,7 @@ return {
     "echasnovski/mini.bufremove",
     -- stylua: ignore
     keys = {
-      { "<leader>c", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
+      { "<leader>C", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
     },
   },
 }
