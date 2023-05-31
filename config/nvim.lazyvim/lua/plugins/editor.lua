8bca09d37e7acc444e26ea4ec82a27c8d66525f0
Akihiro Okuno <choplin.choplin@gmail.com>
2023-05-31 15:57:08 +0900

    Add statuscol.nvim

--- config/nvim.lazyvim/lua/plugins/editor.lua
+++ config/nvim.lazyvim/lua/plugins/editor.lua
@@ -136,4 +136,34 @@ return {
       { "<leader>C", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
     },
   },
+  {
+    "luukvbaal/statuscol.nvim",
+    event = { "VeryLazy" },
+    config = function()
+      local builtin = require("statuscol.builtin")
+      require("statuscol").setup({
+        relculright = true,
+        segments = {
+          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+          {
+            sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+            click = "v:lua.ScSa",
+          },
+          { text = { " ", builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+          {
+            sign = { name = { "Git.*" }, colwidth = 1, auto = true, wrap = true },
+            click = "v:lua.ScSa",
+          },
+          {
+            sign = { name = { "Dap.*" }, colwidth = 1, auto = true, wrap = true },
+            click = "v:lua.ScSa",
+          },
+          {
+            sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+            click = "v:lua.ScSa",
+          },
+        },
+      })
+    end,
+  },
 }
