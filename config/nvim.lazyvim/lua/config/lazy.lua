ee333e856f00496ae4a08dfdf27b748c6289161a
Akihiro Okuno <choplin.choplin@gmail.com>
2023-06-24 12:06:45 +0900

    Use mini-animate

--- config/nvim.lazyvim/lua/config/lazy.lua
+++ config/nvim.lazyvim/lua/config/lazy.lua
@@ -9,6 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "plugins" },
   },
