e4d34950db000c4eb27631821fa8d159050e7303
Akihiro Okuno <choplin.choplin@gmail.com>
2023-06-24 12:06:36 +0900

    Disable default mapping of git-conflict.nvim

--- config/nvim.lazyvim/lua/plugins/git.lua
+++ config/nvim.lazyvim/lua/plugins/git.lua
@@ -15,14 +15,7 @@ return {
     "akinsho/git-conflict.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      default_mappings = {
-        ours = "<leader>co",
-        theirs = "<leader>ct",
-        none = "<leader>c0",
-        both = "<leader>cb",
-        next = "]x",
-        prev = "[x",
-      },
+      default_mappings = false,
     },
   },
   -- Provide fancy UI of git commit
