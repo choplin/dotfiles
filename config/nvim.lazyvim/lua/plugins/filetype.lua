fdbcbca46879dc6691f96b7cb0dbdbf9abd65515
Akihiro Okuno <choplin.choplin@gmail.com>
2023-09-25 15:50:08 +0900

    Disable automatic zig fmt

--- config/nvim.lazyvim/lua/plugins/filetype.lua
+++ config/nvim.lazyvim/lua/plugins/filetype.lua
@@ -4,7 +4,13 @@ return {
   -- Markdown table
   { "dhruvasagar/vim-table-mode", ft = "markdown" },
   -- Zig
-  { "ziglang/zig.vim", ft = "zig" },
+  {
+    "ziglang/zig.vim",
+    ft = "zig",
+    config = function()
+      vim.g.zig_fmt_autosave = 0
+    end,
+  },
   -- Scala
   {
     "scalameta/nvim-metals",
