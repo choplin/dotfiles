e55923ac2dfac7b1ef73a5fbf58bd970c5a68abe
Akihiro Okuno <choplin.choplin@gmail.com>
2023-10-11 00:28:59 +0900

    Use lazyvim.plugins.extras.lang.python to setup python stuff

--- config/nvim.lazyvim/lua/config/lazy.lua
+++ config/nvim.lazyvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.nvim-lint" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
-
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "plugins" },
   },
   defaults = {
