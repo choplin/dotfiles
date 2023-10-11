ddbf1a33d64726288fc778e5452c4eacc4c493cf
Akihiro Okuno <choplin.choplin@gmail.com>
2023-10-11 15:56:19 +0900

    Add debug info to Lualine

--- config/nvim.lazyvim/lua/plugins/lualine.lua
+++ config/nvim.lazyvim/lua/plugins/lualine.lua
@@ -146,6 +146,12 @@ return {
           cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
           color = fg("Constant"),
         },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = fg("Debug"),
+        },
         { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = fg("Special") },
         treesitter(icons),
         lsp(),
