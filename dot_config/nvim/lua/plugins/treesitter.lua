c8f5784aae6d1ded206408a647dcda6f5eb06d99
Akihiro Okuno <choplin.choplin@gmail.com>
2023-10-15 00:34:31 +0900

    Remove plugins that are included by default now

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -37,35 +37,10 @@ return {
       },
     },
   },
-  -- Show current context at the top of the windows as virtual text
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {},
-  },
   -- Show current context at the closed brackets
   {
     "haringsrob/nvim_context_vt",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {},
-  },
-  -- Automatically close html tag
-  {
-    "windwp/nvim-ts-autotag",
-    ft = {
-      "html",
-      "javascript",
-      "typescript",
-      "javascriptreact",
-      "typescriptreact",
-      "svelte",
-      "vue",
-      "tsx",
-      "jsx",
-      "rescript",
-      "xml",
-    },
     opts = {},
   },
   {
