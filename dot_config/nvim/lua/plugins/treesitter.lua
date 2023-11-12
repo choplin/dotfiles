fe1b99ac7e9a1e5e29f4705629f80d9008d237d8
Akihiro Okuno <choplin.choplin@gmail.com>
2023-11-12 16:14:11 +0900

    Add nvim-ts-autotag

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -57,4 +57,19 @@ return {
       })
     end,
   },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = "InsertEnter",
+    opts = {},
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        autotag = {
+          enable = true,
+        },
+      })
+    end,
+  },
 }
