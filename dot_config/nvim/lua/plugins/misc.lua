a00eef671a5fd94bc20552f4f527014065941ba9
Akihiro Okuno <choplin.choplin@gmail.com>
2024-11-28 14:15:20 +0900

    (neogit) use tokyonight

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -3,15 +3,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        require("catppuccin").setup({
-          dim_inactive = {
-            enabled = true,
-          },
-        })
-        require("palette").setup_catpuccin()
-        vim.cmd.colorscheme("catppuccin")
-      end,
+      colorscheme = "tokyonight",
       icons = {
         custom = {
           Tree = "",
@@ -19,6 +11,15 @@ return {
         },
       },
     },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      dim_inactive = true,
+    },
+    init = function()
+      require("palette").setup_tokyonight()
+    end,
   },
   -- Tracking coding activities
   { "wakatime/vim-wakatime", event = "VeryLazy" },
