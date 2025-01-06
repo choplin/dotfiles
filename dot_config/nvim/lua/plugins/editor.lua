a415852434d65742080e8d51b3909611585cf44a
Akihiro Okuno <choplin.choplin@gmail.com>
2025-01-06 17:02:55 +0900

    (neovim) remove statuscol.nvim and use default one by LazyVim

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -77,37 +77,6 @@ return {
       { "<leader>C", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
     },
   },
-  {
-    "luukvbaal/statuscol.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      local builtin = require("statuscol.builtin")
-      require("statuscol").setup({
-        relculright = true,
-        ft_ignore = { "NvimTree", "packer", "dashboard", "help", "startify" },
-        segments = {
-          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-          {
-            sign = { name = { "Diagnostic" }, maxwidth = 1, colwidth = 1, auto = false },
-            click = "v:lua.ScSa",
-          },
-          { text = { " ", builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-          {
-            sign = { namespace = { "gitsigns_extmark_signs_" }, colwidth = 1, auto = true, wrap = true },
-            click = "v:lua.ScSa",
-          },
-          {
-            sign = { name = { "Dap" }, colwidth = 1, auto = true, wrap = true },
-            click = "v:lua.ScSa",
-          },
-          {
-            sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-            click = "v:lua.ScSa",
-          },
-        },
-      })
-    end,
-  },
   -- Make Neovim's fold look modern and keep high performance
   {
     "kevinhwang91/nvim-ufo",
