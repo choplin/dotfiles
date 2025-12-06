77dabc7807685fdf3f3ea06efcf8fbbf532e5cea
Akihiro Okuno <choplin.choplin@gmail.com>
2025-12-06 16:35:42 +0900

    (nvim): <space>e opens yazi at cwd

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -105,14 +105,14 @@ return {
     },
     keys = {
       {
-        "<leader>e",
+        "<leader>E",
         mode = { "n", "v" },
         "<cmd>Yazi<cr>",
         desc = "Open yazi at the current file",
       },
       {
         -- Open in the current working directory
-        "<leader>E",
+        "<leader>e",
         "<cmd>Yazi cwd<cr>",
         desc = "Open the file manager in nvim's working directory",
       },
