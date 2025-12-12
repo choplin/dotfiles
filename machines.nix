6de600fff794fc46f564da38700e62e13a61214b
Akihiro Okuno <choplin.choplin@gmail.com>
2025-12-12 14:26:15 +0900

    (nix): add new machine

--- machines.nix
+++ machines.nix
@@ -5,4 +5,10 @@
     hostname = "Akihiros-MacBook";
     homeDirectory = "/Users/aki";
   };
+  "PC-0085" = {
+    system = "aarch64-darwin";
+    username = "aki";
+    hostname = "PC-0085";
+    homeDirectory = "/Users/aki";
+  };
 }
