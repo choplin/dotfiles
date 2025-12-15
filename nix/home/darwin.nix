35d8d83cd338a2ab33d612a2bd7f6b3fd38ee169
Akihiro Okuno <choplin.choplin@gmail.com>
2025-12-15 20:10:05 +0900

    (nix): install m1ddc only for darwin

--- nix/home/darwin.nix
+++ nix/home/darwin.nix
@@ -1,6 +1,16 @@
-{...}: {
-  targets.darwin.defaults.NSGlobalDomain = {
-    AppleLanguages = ["en-JP" "ja-JP"];
-    AppleLocale = "en_JP";
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  config = lib.mkIf pkgs.stdenv.isDarwin {
+    targets.darwin.defaults.NSGlobalDomain = {
+      AppleLanguages = ["en-JP" "ja-JP"];
+      AppleLocale = "en_JP";
+    };
+
+    packages = with pkgs; [
+      m1ddc
+    ];
   };
 }
