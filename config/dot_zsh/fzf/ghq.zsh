7f54e763702964151f8b3c2d77e82648ba9c2c8d
Akihiro Okuno <choplin.choplin@gmail.com>
2026-01-17 14:51:27 +0900

    feat(zsh): improve ghq-fzf keybindings and add delete function

--- config/dot_zsh/fzf/ghq.zsh
+++ config/dot_zsh/fzf/ghq.zsh
@@ -4,21 +4,40 @@ function __fzf_ghq() {
     local root=$(ghq root)
     local result=$(
         (
-            echo "Enter: cd, Ctrl-O: Insert path" &&
+            echo "Enter: Insert path, Ctrl-C: cd, Ctrl-D: Delete" &&
             ghq list
         ) | fzf --header-lines 1 \
             --select-1 --exit-0 \
             --preview="ls ${root}/{}" \
             --bind 'enter:accept' \
-            --bind 'ctrl-o:print(__insert)+accept')
+            --bind 'ctrl-c:print(__cd)+accept' \
+            --bind 'ctrl-d:print(__delete)+accept')
 
-    if [[ $result == __insert* ]]; then
+    if [[ $result == __cd* ]]; then
         local LF=$'\n'
         local selected_dir=${result#*$LF}
-        LBUFFER+="${root}/${selected_dir}"
-    elif [ -n "$result" ]; then
-        BUFFER="cd ${root}/${result}"
+        BUFFER="cd \"${root}/${selected_dir}\""
         zle accept-line
+    elif [[ $result == __delete* ]]; then
+        local LF=$'\n'
+        local selected_dir=${result#*$LF}
+        local repo_path="${root}/${selected_dir}"
+        local parent_dir=$(dirname "$repo_path")
+        print -n "\nDelete ${selected_dir}? [y/N] "
+        if read -q; then
+            print
+            rm -rf "$repo_path"
+            print "Deleted: ${repo_path}"
+            if [[ -z $(ls -A "$parent_dir") ]]; then
+                rmdir "$parent_dir"
+                print "Deleted: ${parent_dir}"
+            fi
+        else
+            print "\nCancelled."
+        fi
+        zle send-break
+    elif [ -n "$result" ]; then
+        LBUFFER+="${root}/${result}"
     fi
 
     zle reset-prompt
