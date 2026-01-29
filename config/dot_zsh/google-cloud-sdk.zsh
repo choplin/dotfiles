8edf97dbf7c9cf454aacac90b56a59fb39753076
Akihiro Okuno <choplin.choplin@gmail.com>
2026-01-29 15:45:48 +0900

    fix(zsh): fix gcloud cli path

--- config/dot_zsh/google-cloud-sdk.zsh
+++ config/dot_zsh/google-cloud-sdk.zsh
@@ -1,4 +1,4 @@
-local sdk_root=$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+local sdk_root=$HOMEBREW_PREFIX/Caskroom/gcloud-cli/latest/google-cloud-sdk
 if test -d $sdk_root; then
     zsh-defer source $sdk_root/path.zsh.inc
     zsh-defer source $sdk_root/completion.zsh.inc
