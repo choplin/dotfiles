444a4e9436b39c667b164b6b4e005e4778ceaab1
Akihiro Okuno <choplin.choplin@gmail.com>
2026-04-16 13:49:51 +0900

    fix(nvim): update treesitter spec for latest nvim-treesitter API
    nvim-treesitter no longer uses nvim-treesitter.configs module.
    Highlight/indent are now Neovim builtins. Textobjects move keymaps
    use nvim-treesitter-textobjects.move API directly.

--- config/dot_config/nvim/lua/plugins/treesitter.lua
+++ config/dot_config/nvim/lua/plugins/treesitter.lua
@@ -8,66 +8,27 @@ return {
     vim.cmd.packadd("nvim-treesitter-textobjects")
   end,
   after = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "bash",
-        "c",
-        "dockerfile",
-        "go",
-        "html",
-        "java",
-        "javascript",
-        "json",
-        "jsonc",
-        "kotlin",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-        "zig",
-      },
-      textobjects = {
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-            ["]a"] = "@parameter.inner",
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            ["]C"] = "@class.outer",
-            ["]A"] = "@parameter.inner",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-            ["[a"] = "@parameter.inner",
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            ["[C"] = "@class.outer",
-            ["[A"] = "@parameter.inner",
-          },
-        },
-      },
-    })
-
-    -- Enable treesitter-based folding
+    -- Treesitter-based folding
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.opt.foldlevel = 99
+
+    -- Textobjects move keymaps
+    local move = require("nvim-treesitter-textobjects.move")
+    local map = vim.keymap.set
+    local opts = { "n", "x", "o" }
+
+    map(opts, "]f", function() move.goto_next_start("@function.outer") end, { desc = "Next function start" })
+    map(opts, "]F", function() move.goto_next_end("@function.outer") end, { desc = "Next function end" })
+    map(opts, "]c", function() move.goto_next_start("@class.outer") end, { desc = "Next class start" })
+    map(opts, "]C", function() move.goto_next_end("@class.outer") end, { desc = "Next class end" })
+    map(opts, "]a", function() move.goto_next_start("@parameter.inner") end, { desc = "Next parameter start" })
+    map(opts, "]A", function() move.goto_next_end("@parameter.inner") end, { desc = "Next parameter end" })
+    map(opts, "[f", function() move.goto_previous_start("@function.outer") end, { desc = "Previous function start" })
+    map(opts, "[F", function() move.goto_previous_end("@function.outer") end, { desc = "Previous function end" })
+    map(opts, "[c", function() move.goto_previous_start("@class.outer") end, { desc = "Previous class start" })
+    map(opts, "[C", function() move.goto_previous_end("@class.outer") end, { desc = "Previous class end" })
+    map(opts, "[a", function() move.goto_previous_start("@parameter.inner") end, { desc = "Previous parameter start" })
+    map(opts, "[A", function() move.goto_previous_end("@parameter.inner") end, { desc = "Previous parameter end" })
   end,
 }
