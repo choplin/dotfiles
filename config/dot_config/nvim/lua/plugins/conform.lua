40ad99dc7a02a6c3368d6e5cfed4b8a471984a4c
Akihiro Okuno <choplin.choplin@gmail.com>
2026-04-17 22:58:03 +0900

    feat(nvim): add formatter and linter specs
    Add conform.nvim with auto-format on save and formatters for lua, sh,
    java, and python. Add nvim-lint with debounced linting for sh, kotlin,
    and dockerfile.

---
+++ config/dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,38 @@
+return {
+  "conform.nvim",
+  src = "https://github.com/stevearc/conform.nvim",
+  event = "User LazyFile",
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  after = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        sh = { "shfmt" },
+        java = { "google-java-format" },
+        python = { "ruff_fix", "ruff_format" },
+      },
+      default_format_opts = {
+        timeout_ms = 3000,
+        lsp_format = "fallback",
+      },
+    })
+
+    -- Auto-format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("conform_autoformat", { clear = true }),
+      callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+      end,
+    })
+  end,
+}
